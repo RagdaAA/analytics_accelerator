1c4644e2891387a4558b3f03d680ba62d00f2280
Ragda A <155481566+RagdaAA@users.noreply.github.com>
2025-11-12 14:01:35 +0100

    Create Udacity_training.sql

---
+++ Udacity_training.sql
@@ -0,0 +1,14 @@
+SELECT id, occured_at, total_amt_usd
+FROM table_
+ORDER BY date_ ASC
+LIMIT 10;
+
+SELECT id, occured_at, total_amt_usd
+FROM table_
+ORDER BY totsl_amt_usd ASC
+LIMIT 5;
+
+SELECT id, occured_at, total_amt_usd
+FROM table_
+ORDER BY total_amt_usd DESC
+LIMIT 20;
